4d433f5916e303f1118f99cc81fa6fc461dc0e33
ddeepak95 <ddeepak95@gmail.com>
2024-02-29 16:18:38 -0800

    updated content and script

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-  insertTOCElement().then(() => generateTOC());
+  insertHeaderElement();
+  insertTOCElement().then(() => {
+    generateTOC(), makeTOCSticky();
+  });
+  insertFooterElement();
+  createBreadcrumb();
 });
 
 function insertTOCElement() {
@@ -95,4 +100,117 @@ function generateTOC() {
       addActiveClass(topHeading.getAttribute("id"));
     }
   });
+}
+
+function makeTOCSticky() {
+  window.onscroll = function () {
+    var toc = document.querySelector("table-of-contents");
+    if (window.scrollY > 100) {
+      // change 100 to the point you want it to stick
+      toc.classList.add("sticky");
+    } else {
+      toc.classList.remove("sticky");
+    }
+  };
+}
+
+function insertHeaderElement() {
+  const headerHTML = `
+    <header>
+      <div id="stanford-band">
+        <a href="https://www.stanford.edu" target="_blank">Stanford University</a>
+      </div>
+      <div id="cesta-band">
+        <a href="https://cesta.stanford.edu" target="_blank"><span id="cesta-logo-container"><span id="stanford-type">Stanford</span><span id="cesta-expansion">Center for Spatial and Textual Analysis </span></span></a>
+      </div>
+      <nav>
+        <div><a href="/">CESTA Research Anthology</a></div>
+        <div id="menu"><a href="/about">About</a><a href="/submit">Submit</a></div>
+      </nav>
+    </header>
+  `;
+
+  const body = document.querySelector("body");
+  body.insertAdjacentHTML("afterbegin", headerHTML);
+}
+
+function insertFooterElement() {
+  const footerHTML = `
+    <footer>
+      <div class="footer-container">
+        <div class="footer-logo">
+          <a href="https://www.stanford.edu">Stanford<br>University</a>
+        </div>
+        <div class="footer-links">
+          <nav aria-label="global footer menu">
+            <ul class="links-list">
+              <li>
+                <a href="https://www.stanford.edu">Stanford Home</a>
+              </li>
+              <li>
+                <a href="https://visit.stanford.edu/plan/">Maps & Directions</a>
+              </li>
+              <li>
+                <a href="https://www.stanford.edu/search/">Search Stanford</a>
+              </li>
+              <li>
+                <a href="https://emergency.stanford.edu">Emergency Info</a>
+              </li>
+            </ul>
+            <ul class="links-list-secondary">
+              <li>
+                <a href="https://www.stanford.edu/site/terms/" title="Terms of use for sites">Terms of Use</a>
+              </li>
+              <li>
+                <a href="https://www.stanford.edu/site/privacy/" title="Privacy and cookie policy">Privacy</a>
+              </li>
+              <li>
+                <a href="https://uit.stanford.edu/security/copyright-infringement" title="Report alleged copyright infringement">Copyright</a>
+              </li>
+              <li>
+                <a href="https://adminguide.stanford.edu/chapter-1/subchapter-5/policy-1-5-4" title="Ownership and use of Stanford trademarks and images">Trademarks</a>
+              </li>
+              <li>
+                <a href="https://studentservices.stanford.edu/more-resources/student-policies/non-academic/non-discrimination" title="Non-discrimination policy">Non-Discrimination</a>
+              </li>
+              <li>
+                <a href="https://www.stanford.edu/site/accessibility" title="Report web accessibility issues">Accessibility</a>
+              </li>
+            </ul>
+          </nav>
+          <div class="footer-text">
+            <span>© Stanford University. Stanford, California 94305.</span>
+          </div>
+        </div>
+      </div>
+    </footer>
+  `;
+
+  const body = document.querySelector("body");
+  body.insertAdjacentHTML("beforeend", footerHTML);
+}
+
+function createBreadcrumb() {
+  const article = document.querySelector("article");
+  if (!article) return;
+
+  const pageTitle = document.querySelector("article-title").textContent;
+  const urlParts = window.location.pathname.split("/").filter((part) => part);
+  let breadcrumb = '<a href="/" class="breadcrumb-home">Home</a>';
+
+  let currentPath = "";
+  urlParts.forEach((part, index) => {
+    currentPath += "/" + part;
+    if (index < urlParts.length - 1) {
+      const branch = part.charAt(0).toUpperCase() + part.slice(1);
+      breadcrumb += ` <span class="breadcrumb-separator">></span> <a href="${currentPath}" class="breadcrumb-link">${branch}</a>`;
+    }
+  });
+
+  breadcrumb += ` <span class="breadcrumb-separator">></span> <span class="breadcrumb-title">${pageTitle}</span>`;
+
+  const breadcrumbElement = document.createElement("div");
+  breadcrumbElement.className = "breadcrumb";
+  breadcrumbElement.innerHTML = breadcrumb;
+  article.parentNode.insertBefore(breadcrumbElement, article);
 }
